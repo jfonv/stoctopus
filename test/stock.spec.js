ee1986edc903d024f6e9e5205f6d4ff3b8474db2
Joe Fontana <jfonv@allstate.com>
2016-06-17 09:49:56 -0500

    crapgit add .

--- test/stock.spec.js
+++ test/stock.spec.js
@@ -2,8 +2,18 @@
 
 const expect = require('chai').expect;
 const Stock = require('../lib/stock');
+const nock = require('nock');
+// const nock = require('sinon');
 
 describe('Stock', () => {
+  beforeEach(() => {
+    nock('http://dev.markitondemand.com/')
+    .get('/MODApis/Api/v2/Quote/json?symbol=AAPL')
+    .reply(200, {
+      Name: 'Apple',
+      LastPrice: 100,
+    });
+  });
   describe('constructor', () => {
     it('should create new stock', () => {
       // const result = new Stock('Apple, Inc.','AAPL',10,500);
@@ -17,14 +27,15 @@ describe('Stock', () => {
   describe('#purchase', () => {
     it('should purchase stock with provided symbol', (done) => {
       // const result = new Stock('Apple, Inc.','AAPL',10,500);
-      const result = new Stock('AAPL');
+      const result = new Stock('aapl');
       result.purchase(50, (err, totalPaid) => {
         expect(err).to.be.null;
         expect(result.symbol).to.equal('AAPL');
-        expect(totalPaid).to.be.gte(4500);
-        expect(result.name).to.equal('Apple Inc');
+        expect(totalPaid).to.equal(5000);
+        expect(result.name).to.equal('Apple');
         expect(result.shares).to.equal(50);
-        expect(result.pPPS).to.be.gte(90);
+        expect(result.pPPS).to.equal(100);
+        // expect(result.purchaseDate.getTime()).to.equal();
         done();
       });
     });
@@ -36,10 +47,24 @@ describe('Stock', () => {
         result.sell(20, (sellErr, totalSold) => {
           expect(sellErr).to.be.null;
           expect(result.shares).to.equal(30);
-          expect(totalSold).to.be.gte(1500);
+          expect(totalSold).to.equal(2000);
           done();
         });
       });
     });
   });
+  /*
+  describe('#sell', () => {
+    it('should sell stock with provided symbol', (done) => {
+      const result = new Stock('AAPL');
+      result.purchase(50, () => {
+        result.sell(20, (sellErr, totalSold) => {
+          expect(sellErr).to.be.null;
+          expect(result.shares).to.equal(30);
+          expect(totalSold).to.equal(2000);
+          done();
+        });
+      });
+    });
+  });*/
 });
